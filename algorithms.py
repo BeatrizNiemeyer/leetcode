bf994a62a4ec6551add5bfd92a1918b80a477ed7
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-11 14:46:39 -0400

    licenseKeyFormatting

--- algorithms.py
+++ algorithms.py
@@ -207,3 +207,50 @@ def twoSumLessThanK(nums, k):
 
 
 # print(twoSumLessThanK(nums, k))
+
+
+# 482. License Key Formatting
+# Easy
+
+# You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+
+# We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+
+# Return the reformatted license key.
+
+
+# Example 1:
+
+# Input: s = "5F3Z-2e-9-w", k = 4
+# Output: "5F3Z-2E9W"
+# Explanation: The string s has been split into two parts, each part has 4 characters.
+# Note that the two extra dashes are not needed and can be removed.
+
+
+def licenseKeyFormatting(s, k):
+
+    chars = s.split("-")
+    print(chars)
+    str_chars = ""
+
+    for item in chars:
+        str_chars += item
+
+    str_chars = str_chars[::-1]
+
+    result = ""
+
+    count = 0
+
+    for char in str_chars:
+        if count == k:
+            result += "-"
+            count = 0
+
+        result += char.upper()
+        count += 1
+
+    return result[::-1]
+
+
+print(licenseKeyFormatting("2713nh-31sj", 3))
