5c5ad1ee853738e944397107e98338370902579f
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-11 15:02:18 -0400

    sortArrayByParity

--- algorithms.py
+++ algorithms.py
@@ -254,3 +254,36 @@ def licenseKeyFormatting(s, k):
 
 
 print(licenseKeyFormatting("2713nh-31sj", 3))
+
+
+# 905. Sort Array By Parity
+# Easy
+
+# Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+# Return any array that satisfies this condition.
+
+
+# Example 1:
+
+# Input: nums = [3,1,2,4]
+# Output: [2,4,3,1]
+# Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+
+def sortArrayByParity(nums):
+
+    odds = []
+    even = []
+
+    for num in nums:
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odds.append(num)
+
+    return even + odds
