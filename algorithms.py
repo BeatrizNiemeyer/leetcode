acce6b9d39766bf889cb8f97f9631226bf71ccf5
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-15 16:41:24 -0400

    RomanToInteger

--- algorithms.py
+++ algorithms.py
@@ -623,3 +623,62 @@ def isPalindrome(x):
         return True
 
     return False
+
+
+# 13. Roman to Integer
+# Easy
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+
+# Example 1:
+
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+
+
+def romanToInt(s):
+
+    vac = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900,
+    }
+
+    result = 0
+    i = 0
+    while i < len(s):
+        print("im here")
+        double = vac.get(s[i:i+2])
+        single = vac.get(s[i:i+1])
+        print(s[i:i+2])
+        print(s[i:i+1])
+        if double:
+            result += double
+            i += 2
+        elif single:
+            result += single
+            i += 1
+        print(result)
+
+    return result
