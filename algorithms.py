46d7eba08474e9d2ce1c6663ce0bff6d159805f2
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-17 12:06:08 -0400

    IsValid

--- algorithms.py
+++ algorithms.py
@@ -719,3 +719,32 @@ def longestCommonPrefix(strs):
             print("prefix now is ", prefix)
 
         return prefix
+
+
+# 20. Valid Parentheses
+# Easy
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+#     Open brackets must be closed by the same type of brackets.
+#     Open brackets must be closed in the correct order.
+#     Every close bracket has a corresponding open bracket of the same type.
+
+def isValid(s):
+
+    # )(
+
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping:
+            top_element_should_be = stack.pop() if stack else "#"
+            if mapping[char] != top_element_should_be:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
