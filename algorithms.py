0f6087783da8886fdb658e68953f2522c3cab2f9
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-18 15:42:42 -0400

    mySqrt

--- algorithms.py
+++ algorithms.py
@@ -1035,3 +1035,45 @@ def addBinary(a, b):
         carry = carry // 2
 
     return result[::-1]
+
+
+# 69. Sqrt(x)
+# Easy
+
+# Given a non-negative integer x, compute and return the square root of x.
+
+# Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+# Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+
+# Example 2:
+
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+
+
+def mySqrt(x):
+
+    l = []
+
+    if x == 0:
+        return 0
+
+    if x == 1:
+        return 1
+
+    for i in range(x + 1):
+        if (i * i) == x:
+            return i
+        if (i * i) > x:
+            l.append(i - 1)
+            break
+
+    return (l.pop())
