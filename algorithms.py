14e8bf0a7fd3c311777080056d7eeb5e656bf67a
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-19 09:42:10 -0400

    climbStairs

--- algorithms.py
+++ algorithms.py
@@ -1077,3 +1077,34 @@ def mySqrt(x):
             break
 
     return (l.pop())
+
+
+# 70. Climbing Stairs
+# Easy
+
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+
+def climbStairs(n):
+
+    one, two = 1, 1
+
+    0, 1, 2, 3, 4, 5
+
+    for i in range(n - 1):
+        temp = one
+        one = two + one
+        two = temp
+
+    return one
