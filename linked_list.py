dd6ec4f766d0861f0ea445dd18a6513b0f914cec
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-19 11:58:47 -0400

    mergeSortedArray

--- linked_list.py
+++ linked_list.py
@@ -232,3 +232,58 @@ def deleteDuplicates(head):
         curr = curr.next
 
     return head
+
+# 88. Merge Sorted Array
+# Easy
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+# Example 1:
+
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+def merge(nums1, m, nums2, n):
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+
+    # for i in range(n):
+#             nums1[i + m] = nums2[i]
+
+#         # Sort nums1 list in-place.
+#         nums1.sort()
+
+#         j = 0
+#         for i in range(max(len(nums1), len(nums2))):
+#             print(nums1[i])
+#             if nums1[i] == 0 and j < (min(len(nums1), len(nums2))):
+#                 nums1[i] = nums2[j]
+#                 j +=1
+
+#         print(nums1)
+#         nums1.sort()
+
+# [1,2,3,5,6], m = 3,
+# [1, 1,5,6], n = 3
+    last = m + n - 1
+
+    while m > 0 and n > 0:
+        if nums2[n - 1] >= nums1[m - 1]:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+        elif nums1[m - 1] > nums2[n - 1]:
+            nums1[last] = nums1[m - 1]
+            m -= 1
+        last -= 1
+
+    while n > 0:
+        nums1[last] = nums2[n - 1]
+        last, n = last - 1, n - 1
