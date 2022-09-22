49c48de3e6b9810fe3134b16fdaedc74dd427d28
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-22 15:00:15 -0400

    BinarySearch

--- algorithms.py
+++ algorithms.py
@@ -1233,3 +1233,23 @@ def majorityElement(self, nums: List[int]) -> int:
         #         key = nums[i]
                 
         # return key
+
+# 704. Binary Search
+# Easy
+
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+
+def search(nums, target):
+        
+    if target in nums:
+        return nums.index(target)
+    else:
+        return -1
+        
