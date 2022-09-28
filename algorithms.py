dc3809da21c75d1ebe8fbe1dbfe8f8876adbe7fb
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-28 13:00:31 -0400

    wordBreak

--- algorithms.py
+++ algorithms.py
@@ -1589,3 +1589,33 @@ def lengthOfLIS(nums):
                 dp[i] = max(dp[i], 1 + dp[j])
 
     return max(dp)
+
+# 139. Word Break
+# Medium
+
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+# Example 1:
+
+# Input: s = "leetcode", wordDict = ["leet","code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+def wordBreak(s, wordDict):
+
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+
+    for i in range(len(s) - 1, -1, - 1):
+
+        for w in wordDict:
+            print(s[i: i + len(w)], w)
+            if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
+                dp[i] = dp[i + len(w)]
+            if dp[i]:
+                break
+
+    return dp[0]
