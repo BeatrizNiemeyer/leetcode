b99cdda62769b98cea80f65f48b7161bc316e03a
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-09-28 10:00:06 -0400

    lenghOfList

--- algorithms.py
+++ algorithms.py
@@ -1562,3 +1562,30 @@ def coinChange(coins, amount):
                 dp[a] = min(dp[a], 1 + dp[a-c])
 
     return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+# 300. Longest Increasing Subsequence
+# Medium
+
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+# A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+
+# Example 1:
+
+# Input: nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+
+def lengthOfLIS(nums):
+
+    dp = [1] * len(nums)
+
+    for i in range(len(nums) - 1, -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1 + dp[j])
+
+    return max(dp)
