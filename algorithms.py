7fe0a39840139ab64367e682ed8cd130cbcaf82d
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-03 15:52:32 -0400

    MyHashMap

--- algorithms.py
+++ algorithms.py
@@ -1800,3 +1800,40 @@ def groupAnagrams(strs):
         d["".join(sorted(s))].append(s)
 
     return d.values()
+
+# 706. Design HashMap
+
+# Design a HashMap without using any built-in hash table libraries.
+
+# Implement the MyHashMap class:
+
+#     MyHashMap() initializes the object with an empty map.
+#     void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+#     int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+#     void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+
+class MyHashMap:
+
+    def __init__(self):
+        self.d = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.d[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.d:
+            return self.d[key]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.d:
+            del self.d[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
