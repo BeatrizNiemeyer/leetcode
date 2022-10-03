4903b29e44d85998c34401bb9a1c8fefb5d84e3e
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-03 17:01:34 -0400

    sortByBits

--- algorithms.py
+++ algorithms.py
@@ -1837,3 +1837,29 @@ class MyHashMap:
 # obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
+
+
+# 1356. Sort Integers by The Number of 1 Bits
+
+# You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+# Return the array after sorting it.
+
+def sortByBits(arr):
+
+    d = defaultdict(list)
+    l = []
+    for num in arr:
+        a = bin(num)
+        binary = a[2:]
+        d[binary.count("1")].append(num)
+        l.append(binary.count("1"))
+
+    res = []
+    l.sort()
+    for num in set(l):
+        d[num].sort()
+        for item in d[num]:
+            res.append(item)
+
+    return res
