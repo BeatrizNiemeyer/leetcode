3d2e0ec7194b04f018e051830a5033a2e817dac0
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-04 09:38:40 -0400

    reverseList

--- linked_list.py
+++ linked_list.py
@@ -368,3 +368,21 @@ def getIntersectionNode(headA, headB):
     #         return None
     #     else:
     #         cur = cur.next
+
+
+# 206. Reverse Linked List
+# Easy
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+def reverseList(head):
+
+    prev = None
+    curr = head
+
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+
+    return prev
