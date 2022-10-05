9e476b6bb4db0260f33c6aa7ed22310ad1c2edbd
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-05 16:34:40 -0400

    isPalindrome

--- algorithms.py
+++ algorithms.py
@@ -1991,3 +1991,32 @@ def reverse(x):
         return 0
 
     return int(s)
+
+
+# 125. Valid Palindrome
+# Easy
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+def isPalindrome(s):
+
+    if s == " ":
+        return True
+
+    res = ""
+
+    for char in s:
+        if char.isalnum():
+            res += char.lower()
+
+    if res == res[::-1]:
+        return True
