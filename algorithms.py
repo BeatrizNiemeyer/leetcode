96023a37d738d1168594cb7faa6689f002fa5a3d
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-10 20:21:50 -0400

    shortestDistance

--- algorithms.py
+++ algorithms.py
@@ -2288,3 +2288,30 @@ def intToRoman(num):
             i = i-1
         print(num)
     return roman_value
+
+
+# 243. Shortest Word Distance
+
+# Given an array of strings wordsDict and two different strings that already exist in the array word1 and word2, return the shortest distance between these two words in the list.
+
+# Example 1:
+
+# Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "coding", word2 = "practice"
+# Output: 3
+
+
+def shortestDistance(words):
+
+    shortestDistance = len(words)
+    position1, position2 = -1, -1
+    for i in range(len(words)):
+        if words[i] == word1:
+            position1 = i
+        elif words[i] == word2:
+            position2 = i
+
+        if position1 != -1 and position2 != -1:
+            shortestDistance = min(
+                shortestDistance, abs(position1 - position2))
+
+    return shortestDistance
