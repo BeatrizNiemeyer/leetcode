43a00faf4162ffc76683c65e8530c5a690cb7006
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-13 14:04:11 -0400

    strStr

--- algorithms.py
+++ algorithms.py
@@ -2469,3 +2469,20 @@ def permuteUnique(nums):
     dfs()
 
     return res
+
+
+# 28. Find the Index of the First Occurrence in a String
+# Medium
+
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+
+def strStr(haystack, needle):
+
+    return haystack.find(needle)
