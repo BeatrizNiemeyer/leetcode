54434380c3f729c4d935bf2a07cc9454aab2c0ad
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-18 09:38:55 -0400

    getRow

--- algorithms.py
+++ algorithms.py
@@ -2673,3 +2673,31 @@ def longestConsecutive(nums):
             r += 1
         res.append(count + 1)
     return max(res)
+
+
+# 119. Pascal's Triangle II
+# Easy
+
+# Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+# Example 1:
+
+# Input: rowIndex = 3
+# Output: [1,3,3,1]
+
+def getRow(rowIndex):
+
+    l = [[1], [1, 1]]
+    count = 0
+
+    while count <= rowIndex:
+        l2 = []
+        for i in range(len(l[-1])-1):
+            l2.append(l[-1][i] + l[-1][i + 1])
+        l2 = [1] + l2 + [1]
+        l.append(l2)
+        count += 1
+
+    return l[rowIndex]
