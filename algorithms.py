7aeae21a951f2a1f0bf6cd666b482c37ee02baeb
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-18 17:09:31 -0400

    thirsMax

--- algorithms.py
+++ algorithms.py
@@ -2796,3 +2796,27 @@ def summaryRanges(nums):
                 l = []
 
     return res
+
+# 414. Third Maximum Number
+# Easy
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+# Example 1:
+
+# Input: nums = [3,2,1]
+# Output: 1
+# Explanation:
+# The first distinct maximum is 3.
+# The second distinct maximum is 2.
+# The third distinct maximum is 1.
+
+
+def thirdMax(nums):
+    nums = set(nums)
+    nums = list(nums)
+    nums = sorted(nums, reverse=True)
+
+    if len(nums) >= 3:
+        return nums[2]
+    else:
+        return nums[0]
