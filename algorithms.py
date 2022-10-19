1fbbbf2b0891942aef2d4c4b0dde406679a14dc4
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-19 10:35:11 -0400

    maxArray

--- algorithms.py
+++ algorithms.py
@@ -2854,3 +2854,30 @@ def isAnagram(s, t):
 
     if sorted(s) == sorted(t):
         return True
+
+# 53. Maximum Subarray
+# Medium
+
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+# A subarray is a contiguous part of an array.
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+def maxSubArray(nums):
+
+    res = nums[0]
+    summ = 0
+
+    for num in nums:
+        if summ < 0:
+            summ = 0
+        summ += num
+        res = max(res, summ)
+
+    return res
