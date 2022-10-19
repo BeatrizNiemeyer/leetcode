8268bd3d4be5f60d2a3c7c010b4747206b2aa55f
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-19 10:54:18 -0400

    twosum

--- algorithms.py
+++ algorithms.py
@@ -2881,3 +2881,39 @@ def maxSubArray(nums):
         res = max(res, summ)
 
     return res
+
+
+# 167. Two Sum II - Input Array Is Sorted
+# Medium
+
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+# Your solution must use only constant extra space.
+
+def twoSum(nums, target):
+
+    # d = {}
+
+    # for i, n in enumerate(numbers):
+    #     if (target - (n)) in d:
+    #         return([d[target - n], i + 1])
+    #     else:
+    #         d[n] = d.get(n, i +1)
+
+    # [1, 2, 3, 4, 5, 78, 90]  target = 10
+
+    l = 0
+    r = len(nums) - 1
+
+    for num in nums:
+        summ = nums[l] + nums[r]
+        if summ < target:
+            l += 1
+        elif summ > target:
+            r -= 1
+        elif summ == target:
+            return [l+1, r+1]
