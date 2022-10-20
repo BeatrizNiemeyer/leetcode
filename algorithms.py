7ef2a3c396e4a91341a7647af8ddd739ac062527
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-20 16:13:06 -0400

    replaceElements

--- algorithms.py
+++ algorithms.py
@@ -2950,3 +2950,27 @@ def rob(nums):
         prev = curr
         curr = max(x+temp, prev)
     return curr
+
+
+# 1299. Replace Elements with Greatest Element on Right Side
+# Easy
+
+# Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+# After doing so, return the array.
+
+# Example 1:
+
+# Input: arr = [17,18,5,4,6,1]
+# Output: [18,6,6,6,1,-1]
+
+
+def replaceElements(arr):
+
+    for i in range(len(arr)):
+        if i == len(arr) - 1:
+            arr[i] = -1
+        else:
+            arr[i] = max(arr[i+1:])
+
+    return arr
