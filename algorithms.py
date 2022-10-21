ca7d2b316da3d1f7c4a8c35b775c3488390baf70
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-21 11:57:16 -0400

    sortArrayByParityII

--- algorithms.py
+++ algorithms.py
@@ -3092,3 +3092,38 @@ def canConstruct(ransomNote, magazine):
         else:
             return False
     return True
+
+# 922. Sort Array By Parity II
+# Easy
+
+# Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+# Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+# Return any answer array that satisfies this condition.
+
+# Example 1:
+
+# Input: nums = [4,2,5,7]
+# Output: [4,5,2,7]
+# Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+
+def sortArrayByParityII(nums):
+
+    l1 = []
+    l2 = []
+
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            l1.append(nums[i])
+        else:
+            l2.append(nums[i])
+
+    for i in range(len(nums)):
+        if i % 2 == 0:
+            nums[i] = l1.pop()
+        else:
+            nums[i] = l2.pop()
+
+    return nums
