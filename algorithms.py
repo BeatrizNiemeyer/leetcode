bd04aa25aa1711f74402e675f4d451bfc2276321
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-22 13:57:19 -0400

    numberOfSteps

--- algorithms.py
+++ algorithms.py
@@ -3187,3 +3187,29 @@ def missingNumber(arr):
             return arr[i] - max_dif
 
     return arr[0]
+
+# 1342. Number of Steps to Reduce a Number to Zero
+# Easy
+
+# Given an integer num, return the number of steps to reduce it to zero.
+
+# In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it
+
+# Example 1:
+
+# Input: num = 14
+# Output: 6
+
+
+def numberOfSteps(num):
+    count = 0
+
+    while num != 0:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num - 1
+
+        count += 1
+
+    return count
