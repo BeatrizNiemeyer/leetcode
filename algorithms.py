671388a1b680b0d709680006ea66342aeea8f083
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-23 10:09:18 -0400

    CountTime

--- algorithms.py
+++ algorithms.py
@@ -3378,3 +3378,52 @@ def findMaxK(nums):
         return -1
     else:
         return maxi
+
+# 2437. Number of Valid Clock Times
+# Easy
+
+# You are given a string of length 5 called time, representing the current time on a digital clock in the format "hh:mm". The earliest possible time is "00:00" and the latest possible time is "23:59".
+
+# In the string time, the digits represented by the ? symbol are unknown, and must be replaced with a digit from 0 to 9.
+
+# Return an integer answer, the number of valid clock times that can be created by replacing every ? with a digit from 0 to 9.
+
+# Example 1:
+
+# Input: time = "?5:00"
+# Output: 2
+# Explanation: We can replace the ? with either a 0 or 1, producing "05:00" or "15:00". Note that we cannot replace it with a 2, since the time "25:00" is invalid. In total, we have two choices.
+
+
+def countTime(time):
+
+    res = 1
+
+    if time[0] == "?":
+        if time[0] == "?" and time[1] == "?":
+            res *= 24
+        elif time[0] == "?" and int(time[1]) >= 4:
+            res *= 2
+        elif time[0] == "?" and int(time[1]) < 4:
+            res *= 3
+
+    if time[0] == "?":
+        for i in range(3, len(time)):
+            if time[i] == "?":
+                if i == 3:
+                    res *= 6
+                elif i == 4:
+                    res *= 10
+    else:
+        for i in range(1, len(time)):
+            if time[i] == "?":
+                if i == 1 and int(time[0]) < 2:
+                    res *= 10
+                elif i == 1 and int(time[0]) == 2:
+                    res *= 4
+                elif i == 3:
+                    res *= 6
+                elif i == 4:
+                    res *= 10
+
+    return res
