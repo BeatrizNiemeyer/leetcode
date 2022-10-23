22ff097534950e383afe41e74be21511116a3338
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-23 10:16:54 -0400

    commonFactors

--- algorithms.py
+++ algorithms.py
@@ -3427,3 +3427,29 @@ def countTime(time):
                     res *= 10
 
     return res
+
+# 2427. Number of Common Factors
+# Easy
+
+# Given two positive integers a and b, return the number of common factors of a and b.
+
+# An integer x is a common factor of a and b if x divides both a and b.
+
+# Example 1:
+
+# Input: a = 12, b = 6
+# Output: 4
+# Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+
+
+def commonFactors(a, b):
+
+    count = 0
+
+    min_num = min(a, b)
+
+    for i in range(1, min_num + 1):
+        if a % i == 0 and b % i == 0:
+            count += 1
+
+    return count
