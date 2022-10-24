b06508e4bbd8168f7bb9397a2f01a7132ff67217
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-24 13:48:20 -0400

    sortPeople

--- algorithms.py
+++ algorithms.py
@@ -3497,3 +3497,42 @@ def minDeletions(self, s: str) -> int:
             seen.add(value)
 
     return count
+
+# 2418. Sort the People
+# Easy
+
+# You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+# For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+# Return names sorted in descending order by the people's heights.
+
+# Example 1:
+
+# Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+# Output: ["Mary","Emma","John"]
+# Explanation: Mary is the tallest, followed by Emma and John.
+
+
+def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+    """
+
+    create a dictionary where key = height and value = name
+    crete a list that will be my output
+    sort the array heights in reverse order
+    loop over my dicitonary, comparing the sorted heights with the value of dicitonary
+    """
+
+    d = {}
+
+    for i in range(len(names)):
+        d[heights[i]] = names[i]
+
+    output = []
+
+    sorted_list = sorted(heights, reverse=True)
+
+    for height in sorted_list:
+        output.append(d[height])
+
+    return output
