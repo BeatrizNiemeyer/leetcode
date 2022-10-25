befd3f2923fee87b5bd2c7ef1e3454e4e1ac0cf0
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-25 13:51:18 -0400

    searchInsert-betterVersion

--- algorithms.py
+++ algorithms.py
@@ -3546,3 +3546,34 @@ def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         output.append(d[height])
 
     return output
+
+# 35. Search Insert Position
+# Easy
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+
+def searchInsert(self, nums: List[int], target: int) -> int:
+
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] > target:
+            r = mid - 1
+
+        elif nums[mid] < target:
+            l = mid + 1
+
+    return l
