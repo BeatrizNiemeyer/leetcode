91c5560f7b8be6a618d98ff6dca1208d2298b7b0
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-29 10:33:24 -0400

    finalValue

--- algorithms.py
+++ algorithms.py
@@ -3877,3 +3877,47 @@ def defangIPaddr(self, address: str) -> str:
             new_ip += address[i]
 
     return new_ip
+
+
+2011. Final Value of Variable After Performing Operations
+Easy
+
+There is a programming language with only four operations and one variable X:
+
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+Example 1:
+
+Input: operations = ["--X", "X++", "X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X = 0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+
+
+def finalValueAfterOperations(self, operations: List[str]) -> int:
+
+    # output = 0
+
+    # for item in operations:
+    #     if item == "--X" or item == "X--":
+    #         output -= 1
+    #     else:
+    #         output += 1
+
+    # return output
+
+    X = 0
+    for op in operations:
+        if "+" in op:
+            X += 1
+        elif "-" in op:
+            X -= 1
+    return X
