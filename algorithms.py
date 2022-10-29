9356cc5bc9aeca60e7fdd10734f38fcffefd5815
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-10-29 12:01:10 -0400

    ngoodPairs

--- algorithms.py
+++ algorithms.py
@@ -3978,3 +3978,28 @@ def mostWordsFound(self, sentences: List[str]) -> int:
             max_nums_of_words = sentence_len
 
     return max_nums_of_words
+
+# 1512. Number of Good Pairs
+# Easy
+
+# Given an array of integers nums, return the number of good pairs.
+
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+
+def numIdenticalPairs(self, nums: List[int]) -> int:
+
+    counter = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                counter += 1
+
+    return counter
