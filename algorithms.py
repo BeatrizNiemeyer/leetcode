a45cea83ae60c67c6fe3e1d7f8dd2e64162eb40d
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-01 10:29:21 -0400

    subtractProductAndSum

--- algorithms.py
+++ algorithms.py
@@ -4117,3 +4117,34 @@ def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
             output.append(False)
 
     return output
+
+
+# 1281. Subtract the Product and Sum of Digits of an Integer
+# Easy
+
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+# Example 1:
+
+# Input: n = 234
+# Output: 15
+# Explanation:
+# Product of digits = 2 * 3 * 4 = 24
+# Sum of digits = 2 + 3 + 4 = 9
+# Result = 24 - 9 = 15
+
+def subtractProductAndSum(self, n: int) -> int:
+
+    # n % 10 -> last digit
+    #  n // 10 -> takes the last digit away
+
+    mult = 1
+    summ = 0
+
+    while n != 0:
+        last = n % 10
+        summ += last
+        mult *= last
+        n = n // 10
+
+    return (mult - summ)
