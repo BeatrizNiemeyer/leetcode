63b8de7ddf7e360b84ff673cff8f7f076583c2af
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-01 12:03:57 -0400

    compress

--- algorithms.py
+++ algorithms.py
@@ -4199,3 +4199,58 @@ def decompressRLElist(self, nums: List[int]) -> List[int]:
         output.extend((nums[i] * [nums[i + 1]]))
 
     return output
+
+
+# 43. String Compression
+# Medium
+
+# Given an array of characters chars, compress it using the following algorithm:
+
+# Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+#     If the group's length is 1, append the character to s.
+#     Otherwise, append the character followed by the group's length.
+
+# The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+# After you are done modifying the input array, return the new length of the array.
+
+# You must write an algorithm that uses only constant extra space.
+
+
+# Example 1:
+
+# Input: chars = ["a","a","b","b","c","c","c"]
+# Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+# Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+
+def compress(self, chars: List[str]) -> int:
+
+    # if len(chars) == 1: return 1
+    # start looping over second element -> if there is more than one element. loop over entire list
+    # count = 1
+    # is element == output[-1] ? YES -> count +=1, NO -> add count, add element to output, count == 1
+    # if element is the last one, add count if is > 1
+    # return len(output)
+
+    if len(chars) == 1:
+        return 1
+
+    output = [chars[0]]
+    count = 1
+
+    for i in range(1, len(chars)):
+        if chars[i] == output[-1]:
+            count += 1
+        else:
+            if count > 1:
+                output.extend(list(str(count)) + [chars[i]])
+                count = 1
+            else:
+                output.append(chars[i])
+        if i == len(chars) - 1 and count > 1:
+            output.extend(list(str(count)))
+
+    chars[:] = output
+
+    return len(output)
