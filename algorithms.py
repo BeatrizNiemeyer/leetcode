ecfd917b7b161a3ebdb7e9c556d751f8bcd5ab1b
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-15 09:57:55 -0500

    minPastitions

--- algorithms.py
+++ algorithms.py
@@ -4393,3 +4393,21 @@ def frequencySort(self, nums: List[int]) -> List[int]:
     for num, freq in r:
         output.extend(freq * [num])
     return output
+
+# 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+# Medium
+
+# A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+
+# Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+
+# Example 1:
+
+# Input: n = "32"
+# Output: 3
+# Explanation: 10 + 11 + 11 = 32
+
+
+def minPartitions(self, n: str) -> int:
+
+    return max(n)
