e852887e8da0cc5cc12ca03bbe8e84254f9441f7
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-16 10:21:39 -0500

    sumOddLenghtSubarrays

--- algorithms.py
+++ algorithms.py
@@ -4625,3 +4625,40 @@ def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
                 count += 1
 
     return count
+
+
+# 1588. Sum of All Odd Length Subarrays
+# Easy
+
+# Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+# A subarray is a contiguous subsequence of the array.
+
+# Example 1:
+
+# Input: arr = [1,4,2,5,3]
+# Output: 58
+# Explanation: The odd-length subarrays of arr and their sums are:
+# [1] = 1
+# [4] = 4
+# [2] = 2
+# [5] = 5
+# [3] = 3
+# [1,4,2] = 7
+# [4,2,5] = 11
+# [2,5,3] = 10
+# [1,4,2,5,3] = 15
+# If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+
+def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+
+    # [1,4,2,5,3]
+
+    res = 0
+    for i in range(len(arr)):
+        s = 0
+        for j in range(i, len(arr)):
+            s += arr[j]
+            if (j-i) % 2 == 0:  # checking even because indexes starts from 0 as its even
+                res += s
+    return res
