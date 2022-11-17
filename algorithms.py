50003d724f13ddfd3501fcf5dd5db91167702ad5
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-17 14:07:23 -0500

    targetIndices

--- algorithms.py
+++ algorithms.py
@@ -4921,3 +4921,30 @@ def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
                 item[i] = 1
 
     return image
+
+
+# 2089. Find Target Indices After Sorting Array
+# Easy
+
+# You are given a 0-indexed integer array nums and a target element target.
+
+# A target index is an index i such that nums[i] == target.
+
+# Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+# Example 1:
+
+# Input: nums = [1,2,5,2,3], target = 2
+# Output: [1,2]
+# Explanation: After sorting, nums is [1,2,2,3,5].
+# The indices where nums[i] == 2 are 1 and 2.
+
+def targetIndices(self, nums: List[int], target: int) -> List[int]:
+
+    l = []
+    nums.sort()
+    for i in range(len(nums)):
+        if nums[i] == target:
+            l.append(i)
+
+    return l
