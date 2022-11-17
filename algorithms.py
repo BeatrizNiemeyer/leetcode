c4d5b7a7014057d731323237033dcd0f23873df6
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-17 10:30:50 -0500

    reverseWords

--- algorithms.py
+++ algorithms.py
@@ -4834,3 +4834,24 @@ def maximum69Number(self, num: int) -> int:
         num[i] = "9"
 
     return "".join(num)
+
+# 557. Reverse Words in a String III
+# Easy
+
+# Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+# Example 1:
+
+# Input: s = "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
+
+
+def reverseWords(self, s: str) -> str:
+
+    t = s.split(" ")
+    r = []
+
+    for item in t:
+        r.append(item[::-1])
+
+    return " ".join(r)
