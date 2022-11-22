1707c91472a3654520b193484a191623a38477ff
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-22 15:54:27 -0500

    numberofMATCHES

--- algorithms.py
+++ algorithms.py
@@ -5056,3 +5056,31 @@ def countAsterisks(self, s: str) -> int:
         output += split_s[i].count("*")
 
     return output
+
+
+# 1688. Count of Matches in Tournament
+# Easy
+
+# You are given an integer n, the number of teams in a tournament that has strange rules:
+
+#     If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+#     If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+
+# Return the number of matches played in the tournament until a winner is decided.
+
+def numberOfMatches(self, n: int) -> int:
+
+    res = 0
+
+    while True:
+        if n % 2 == 0:
+            team = n // 2
+        else:
+            team = (n // 2) + 1
+        matches = n // 2
+        res += matches
+        n = team
+        if n == 1:
+            break
+
+    return res
