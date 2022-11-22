0b024b0a622ebfe49c71c79186553d09028cabd8
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-22 15:33:29 -0500

    countKDiferrence

--- algorithms.py
+++ algorithms.py
@@ -4996,3 +4996,35 @@ def decode(self, encoded: List[int], first: int) -> List[int]:
         first = temp
 
     return res
+
+# 2006. Count Number of Pairs With Absolute Difference K
+# Easy
+
+# Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+# The value of |x| is defined as:
+
+#     x if x >= 0.
+#     -x if x < 0.
+
+# Example 1:
+
+# Input: nums = [1,2,2,1], k = 1
+# Output: 4
+# Explanation: The pairs with an absolute difference of 1 are:
+# - [1,2,2,1]
+# - [1,2,2,1]
+# - [1,2,2,1]
+# - [1,2,2,1]
+
+
+def countKDifference(self, nums: List[int], k: int) -> int:
+
+    res = 0
+
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if abs(nums[i] - nums[j]) == k:
+                res += 1
+
+    return res
