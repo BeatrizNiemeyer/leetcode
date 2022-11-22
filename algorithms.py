e3abe47976aa826d930ef80019d496a412b25911
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-22 15:40:57 -0500

    countAsterisks

--- algorithms.py
+++ algorithms.py
@@ -5028,3 +5028,31 @@ def countKDifference(self, nums: List[int], k: int) -> int:
                 res += 1
 
     return res
+
+
+# 2315. Count Asterisks
+# Easy
+
+# You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+# Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+# Note that each '|' will belong to exactly one pair.
+
+# Example 1:
+
+# Input: s = "l|*e*et|c**o|*de|"
+# Output: 2
+# Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
+# The characters between the first and second '|' are excluded from the answer.
+# Also, the characters between the third and fourth '|' are excluded from the answer.
+# There are 2 asterisks considered. Therefore, we return 2.
+
+def countAsterisks(self, s: str) -> int:
+    output = 0
+
+    split_s = s.split("|")
+    for i in range(0, len(split_s), 2):
+        output += split_s[i].count("*")
+
+    return output
