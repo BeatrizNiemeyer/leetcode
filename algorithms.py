66c033a363553e6d5b1198f20079f1b0bcbd5946
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-28 18:42:53 -0500

    replaceDigits

--- algorithms.py
+++ algorithms.py
@@ -5252,3 +5252,45 @@ def countPairs(self, nums: List[int], k: int) -> int:
                 count += 1
 
     return count
+
+
+# 1844. Replace All Digits with Characters
+# Easy
+
+# You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
+
+# There is a function shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
+
+#     For example, shift('a', 5) = 'f' and shift('x', 0) = 'x'.
+
+# For every odd index i, you want to replace the digit s[i] with shift(s[i-1], s[i]).
+
+# Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
+
+# Example 1:
+
+# Input: s = "a1c1e1"
+# Output: "abcdef"
+# Explanation: The digits are replaced as follows:
+# - s[1] -> shift('a',1) = 'b'
+# - s[3] -> shift('c',1) = 'd'
+# - s[5] -> shift('e',1) = 'f'
+
+def replaceDigits(self, s: str) -> str:
+
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    new_s = ""
+
+    for i in range(len(s)):
+        if s[i].isnumeric():
+            find_index = alpha.find(new_s[-1])
+            correct_char = alpha[find_index + int(s[i])]
+            new_s += correct_char
+
+        else:
+            new_s += s[i]
+
+    s = new_s
+
+    return s
