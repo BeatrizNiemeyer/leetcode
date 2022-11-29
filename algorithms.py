d399dff4f5aaf975ac30ce112230c21c1269cb8e
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-11-29 18:45:22 -0500

    arraysIntersection

--- algorithms.py
+++ algorithms.py
@@ -5319,3 +5319,25 @@ def maxProductDifference(self, nums: List[int]) -> int:
     nums.sort()
 
     return abs((nums[0] * nums[1]) - (nums[-2] * nums[-1]))
+
+# 1213. Intersection of Three Sorted Arrays
+# Easy
+
+# Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, return a sorted array of only the integers that appeared in all three arrays.
+
+# Example 1:
+
+# Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+# Output: [1,5]
+# Explanation: Only 1 and 5 appeared in the three arrays.
+
+
+def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+
+    output = []
+
+    for num in arr1:
+        if num in arr2 and num in arr3:
+            output.append(num)
+
+    return output
