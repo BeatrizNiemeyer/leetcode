4904f73a8d554d9bf071d53116bd1cae5e8ba4c1
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-01 09:43:29 -0500

    freqAplhabeths

--- algorithms.py
+++ algorithms.py
@@ -5396,3 +5396,44 @@ def numOfStrings(self, patterns: List[str], word: str) -> int:
         if w in word:
             count += 1
     return count
+
+# 1309. Decrypt String from Alphabet to Integer Mapping
+# Easy
+
+# You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+#     Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+#     Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+
+# Return the string formed after mapping.
+
+# The test cases are generated so that a unique mapping will always exist.
+
+# Example 1:
+
+# Input: s = "10#11#12"
+# Output: "jkab"
+# Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+
+def freqAlphabets(self, s: str) -> str:
+    output = ""
+    d = {
+        1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h", 9: "i", 10: "j", 11: "k", 12: "l",
+        13: "m", 14: "n", 15: "o", 16: "p", 17: "q", 18: "r", 19: "s", 20: "t", 21: "u", 22: "v", 23: "w",
+        24: "x", 25: "y", 26: "z",
+    }
+
+    j = len(s) - 1
+    while j >= 0:
+        if s[j] == "#":
+            num = s[j - 2: j]
+            print(num)
+            output += d[int(num)]
+            j -= 3
+        else:
+            print(s[j])
+            output += d[int(s[j])]
+            j -= 1
+
+    return output[::-1]
