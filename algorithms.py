3159f677d65abeb7ccd02412898fdf5aaab726ff
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-06 15:47:14 -0500

    pivotInteger

--- algorithms.py
+++ algorithms.py
@@ -5529,3 +5529,30 @@ def countPoints(self, rings: str) -> int:
             count += 1
 
     return count
+
+
+# 2485. Find the Pivot Integer
+# Easy
+
+# Given a positive integer n, find the pivot integer x such that:
+
+# The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+
+# Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+
+# Example 1:
+
+# Input: n = 8
+# Output: 6
+# Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+
+def pivotInteger(self, n: int) -> int:
+
+    l = [x for x in range(1, n + 1)]
+
+    res = -1
+    for i in range(len(l) + 1):
+        if sum(l[0:i + 1]) == sum(l[i:]):
+            res = l[i]
+
+    return res
