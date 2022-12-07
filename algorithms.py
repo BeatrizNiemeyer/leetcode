1cfef25ab00b95d2afc52d84c2b203f1c9eb4f09
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-07 11:55:37 -0500

    removeOuterParentheses

--- algorithms.py
+++ algorithms.py
@@ -5556,3 +5556,45 @@ def pivotInteger(self, n: int) -> int:
             res = l[i]
 
     return res
+
+
+# 1021. Remove Outermost Parentheses
+# Easy
+
+# A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+#     For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+# A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+# Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+# Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+# Example 1:
+
+# Input: s = "(()())(())"
+# Output: "()()()"
+# Explanation:
+# The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+# After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+
+def removeOuterParentheses(self, s: str) -> str:
+
+    l, r = 0, 0
+    cur = ""
+    res = ""
+
+    for char in s:
+        cur += char
+        if char == "(":
+            l += 1
+        elif char == ")":
+            r += 1
+
+        if l == r:
+            res += cur[1:-1]
+            cur = ""
+
+    return res
