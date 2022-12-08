329519549dbb32e2689d1c9146b94e407de54e91
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-08 14:03:08 -0500

    selfDividingNumbers

--- algorithms.py
+++ algorithms.py
@@ -5628,3 +5628,38 @@ def countLetters(self, s: str) -> int:
                 count += 1
 
     return count
+
+# 728. Self Dividing Numbers
+# Easy
+
+# A self-dividing number is a number that is divisible by every digit it contains.
+
+#     For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+# A self-dividing number is not allowed to contain the digit zero.
+
+# Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+
+# Example 1:
+
+# Input: left = 1, right = 22
+# Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+
+def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+
+    l = []
+    divisible = False
+    for num in range(left, right + 1):
+        l_num = list(str(num))
+        for n in l_num:
+            if "0" in n or num % int(n) != 0:
+                divisible = False
+            elif num % int(n) == 0:
+                divisible = True
+            if divisible == False:
+                break
+        if divisible == True:
+            l.append(num)
+
+    return l
