d733a510062f84a72fe9e0342aacf80a7152b205
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-09 10:25:47 -0500

    findNumebers

--- algorithms.py
+++ algorithms.py
@@ -5663,3 +5663,31 @@ def selfDividingNumbers(self, left: int, right: int) -> List[int]:
             l.append(num)
 
     return l
+
+# 1295. Find Numbers with Even Number of Digits
+# Easy
+
+# Given an array nums of integers, return how many of them contain an even number of digits.
+
+# Example 1:
+
+# Input: nums = [12,345,2,6,7896]
+# Output: 2
+# Explanation:
+# 12 contains 2 digits (even number of digits).
+# 345 contains 3 digits (odd number of digits).
+# 2 contains 1 digit (odd number of digits).
+# 6 contains 1 digit (odd number of digits).
+# 7896 contains 4 digits (even number of digits).
+# Therefore only 12 and 7896 contain an even number of digits.
+
+
+def findNumbers(self, nums: List[int]) -> int:
+
+    count = 0
+
+    for num in nums:
+        if len(str(num)) % 2 == 0:
+            count += 1
+
+    return count
