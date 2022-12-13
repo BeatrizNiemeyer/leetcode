a9f8d5e34af30df82b9946a64e24a1fecb0ee375
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-13 12:08:47 -0500

    deleteGreatestValue

--- algorithms.py
+++ algorithms.py
@@ -5725,3 +5725,44 @@ def diStringMatch(self, s: str) -> List[int]:
 
     res.append(l)
     return res
+
+# 2500. Delete Greatest Value in Each Row
+# Easy
+
+# You are given an m x n matrix grid consisting of positive integers.
+
+# Perform the following operation until grid becomes empty:
+
+#     Delete the element with the greatest value from each row. If multiple such elements exist, delete any of them.
+#     Add the maximum of deleted elements to the answer.
+
+# Note that the number of columns decreases by one after each operation.
+
+# Return the answer after performing the operations described above.
+
+# Example 1:
+
+# Input: grid = [[1,2,4],[3,3,1]]
+# Output: 8
+# Explanation: The diagram above shows the removed values in each step.
+# - In the first operation, we remove 4 from the first row and 3 from the second row (notice that, there are two cells with value 3 and we can remove any of them). We add 4 to the answer.
+# - In the second operation, we remove 2 from the first row and 3 from the second row. We add 3 to the answer.
+# - In the third operation, we remove 1 from the first row and 1 from the second row. We add 1 to the answer.
+# The final answer = 4 + 3 + 1 = 8.
+
+
+def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+
+    total = 0
+
+    for lst in grid:
+        lst.sort()
+        lst.reverse()
+
+    for i in range(len(grid[0])):
+        max_n = 0
+        for j in range(len(grid)):
+            max_n = max(max_n, grid[j][i])
+        total += max_n
+
+    return total
