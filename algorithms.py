6c87041130356b05c6228707d99b8e96cec73225
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-14 18:08:03 -0500

    generateTheString

--- algorithms.py
+++ algorithms.py
@@ -5789,3 +5789,25 @@ def firstPalindrome(self, words: List[str]) -> str:
             return word
 
     return ""
+
+# 1374. Generate a String With Characters That Have Odd Counts
+# Easy
+
+# Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+# The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.
+
+# Example 1:
+
+# Input: n = 4
+# Output: "pppz"
+# Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+
+
+def generateTheString(self, n: int) -> str:
+
+    if n % 2 != 0:
+        return "a" * n
+    else:
+        s = ("a" * (n - 1)) + "b"
+        return s
