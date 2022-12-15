0b7bbc6828f97a8b0d129171f8a7343b53960857
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-15 18:51:07 -0500

    minOperations

--- algorithms.py
+++ algorithms.py
@@ -5835,3 +5835,38 @@ def reversePrefix(self, word: str, ch: str) -> str:
         i = word.find(ch)
         return word[:i + 1][::-1] + word[i + 1:]
     return word
+
+
+# 1827. Minimum Operations to Make the Array Increasing
+# Easy
+# 844
+# 39
+# Companies
+
+# You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+
+#     For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+
+# Return the minimum number of operations needed to make nums strictly increasing.
+
+# An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+# Example 1:
+
+# Input: nums = [1,1,1]
+# Output: 3
+# Explanation: You can do the following operations:
+# 1) Increment nums[2], so nums becomes [1,1,2].
+# 2) Increment nums[1], so nums becomes [1,2,2].
+# 3) Increment nums[2], so nums becomes [1,2,3].
+
+ def minOperations(self, nums: List[int]) -> int:
+
+        res =0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i- 1]:
+                subt = (nums[i - 1] + 1) - nums[i] 
+                res += subt
+                nums[i] = (nums[i - 1] + 1)
+
+        return res
