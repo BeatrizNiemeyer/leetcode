277f2e6f5c9d74e14c790210fb20e2c03720f0f0
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-16 18:32:14 -0500

    prefixCount

--- algorithms.py
+++ algorithms.py
@@ -5870,3 +5870,30 @@ def reversePrefix(self, word: str, ch: str) -> str:
                 nums[i] = (nums[i - 1] + 1)
 
         return res
+
+# 2185. Counting Words With a Given Prefix
+
+# You are given an array of strings words and a string pref.
+
+# Return the number of strings in words that contain pref as a prefix.
+
+# A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+# Example 1:
+
+# Input: words = ["pay","attention","practice","attend"], pref = "at"
+# Output: 2
+# Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+def prefixCount(self, words: List[str], pref: str) -> int:
+
+    count = 0
+    n = len(pref)
+
+    for word in words:
+        if pref == word[:n]:
+            count += 1
+
+    return count
