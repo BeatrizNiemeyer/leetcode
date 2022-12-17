209b5d7893e5947a23376ce1666ee740b0fec51a
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-17 14:21:11 -0500

    findDG

--- algorithms.py
+++ algorithms.py
@@ -5897,3 +5897,31 @@ def prefixCount(self, words: List[str], pref: str) -> int:
             count += 1
 
     return count
+
+# 1979. Find Greatest Common Divisor of Array
+# Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+# The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+# Example 1:
+
+# Input: nums = [2,5,6,9,10]
+# Output: 2
+# Explanation:
+# The smallest number in nums is 2.
+# The largest number in nums is 10.
+# The greatest common divisor of 2 and 10 is 2.
+
+def findGCD(self, nums: List[int]) -> int:
+
+
+        min_n = min(nums)
+        max_n = max(nums)
+
+        divisor = 1
+
+        for i in range(max_n  + 1, 1, -1):
+            if min_n % i == 0 and max_n  % i == 0:
+                return i
+
+        return divisor
