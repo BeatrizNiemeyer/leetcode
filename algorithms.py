ca61f232a3e1ce19d8dd00c179116784d4c5ed3f
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2022-12-21 18:30:31 -0500

    isSameAfterReversals

--- algorithms.py
+++ algorithms.py
@@ -6019,3 +6019,24 @@ def numberOfPairs(self, nums: List[int]) -> List[int]:
         leftover += value % 2 
     
     return [pairs, leftover]
+
+# 2119. A Number After a Double Reversal
+
+# Reversing an integer means to reverse all its digits.
+
+#     For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+
+# Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+# Example 1:
+
+# Input: num = 526
+# Output: true
+
+def isSameAfterReversals(self, num: int) -> bool:
+
+    num = str(num)
+
+    if len(num) > 1 and num[-1] == "0":
+        return False
+    return True
