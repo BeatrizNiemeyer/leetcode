e1b9b1dc78519d3abdabf5fcfb3c3c8684c7fa10
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-01-16 16:14:13 -0500

    percentageLetter

--- algorithms.py
+++ algorithms.py
@@ -6129,3 +6129,22 @@ def squareIsWhite(self, coordinates: str) -> bool:
         return True
     else:
         False
+
+
+# 2278. Percentage of Letter in String
+
+# Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+
+# Example 1:
+
+# Input: s = "foobar", letter = "o"
+# Output: 33
+# Explanation:
+# The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
+
+def percentageLetter(self, s: str, letter: str) -> int:
+
+        lst_s = list(s)
+        frequency = lst_s.count(letter)
+        return int(frequency / len(s) * 100)
+
