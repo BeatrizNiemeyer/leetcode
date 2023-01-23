ed7318ccd1328b15ccd8f24f1164135f7e1eb384
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-01-23 15:42:31 -0500

    differenceOfSum

--- algorithms.py
+++ algorithms.py
@@ -6235,3 +6235,39 @@ def countOperations(self, num1: int, num2: int) -> int:
         count +=1
     
     return count
+
+
+# 2535. Difference Between Element Sum and Digit Sum of an Array
+
+# You are given a positive integer array nums.
+
+#     The element sum is the sum of all the elements in nums.
+#     The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+
+# Return the absolute difference between the element sum and digit sum of nums.
+
+# Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+# Example 1:
+
+# Input: nums = [1,15,6,3]
+# Output: 9
+# Explanation: 
+# The element sum of nums is 1 + 15 + 6 + 3 = 25.
+# The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+# The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
+def differenceOfSum(self, nums: List[int]) -> int:
+
+    sum1 = sum(nums)
+
+    summing = 0
+    for num in nums:
+        if len(str(num)) > 1:
+            for n in (list(str(num))):
+                summing += int(n)
+        else:
+            summing += num
+        
+    return sum1 - summing
+
