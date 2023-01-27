13f7e7a30552a8ad10381588a9be4993d07e2f8c
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-01-27 18:42:09 -0500

    answerQueries

--- algorithms.py
+++ algorithms.py
@@ -6388,3 +6388,38 @@ def minTimeToType(self, word: str) -> int:
         ans += min(val, 26 - val)
         prev = ch
     return ans 
+
+# 2389. Longest Subsequence With Limited Sum
+
+# You are given an integer array nums of length n, and an integer array queries of length m.
+
+# Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+
+# A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+
+# Example 1:
+
+# Input: nums = [4,5,2,1], queries = [3,10,21]
+# Output: [2,3,4]
+# Explanation: We answer the queries as follows:
+# - The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+# - The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+# - The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+
+def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+
+    ans=[]
+    nums.sort()
+    for q in queries:
+        s=0 #sum of subsequence
+        l=0 #length of subsequence
+        for i in nums:
+            if s+i<=q: #check before adding the element.
+                s+=i
+                l+=1
+            else:
+                break
+        ans.append(l)
+    return ans
+
