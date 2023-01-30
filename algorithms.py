1f01686cf47ce3adda4d2ff0dfa519c9864d9329
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-01-30 15:59:43 -0500

    kWeakestRows

--- algorithms.py
+++ algorithms.py
@@ -6423,3 +6423,54 @@ def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         ans.append(l)
     return ans
 
+
+# 1337. The K Weakest Rows in a Matrix
+
+# You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+# A row i is weaker than a row j if one of the following is true:
+
+#     The number of soldiers in row i is less than the number of soldiers in row j.
+#     Both rows have the same number of soldiers and i < j.
+
+# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+# Example 1:
+
+# Input: mat = 
+# [[1,1,0,0,0],
+#  [1,1,1,1,0],
+#  [1,0,0,0,0],
+#  [1,1,0,0,0],
+#  [1,1,1,1,1]], 
+# k = 3
+# Output: [2,0,3]
+# Explanation: 
+# The number of soldiers in each row is: 
+# - Row 0: 2 
+# - Row 1: 4 
+# - Row 2: 1 
+# - Row 3: 2 
+# - Row 4: 5 
+# The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+
+    pairs = []
+
+    for i in range(len(mat)):
+        sum_el = sum(mat[i])
+        pairs.append((i, sum_el))
+
+    pairs.sort(key=lambda item:item[1])
+    
+    output = []
+    count = 0
+
+    for index, soldier in pairs:
+        if count < k:
+            output.append(index)
+            count += 1
+
+
+    return output
