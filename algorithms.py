d76ba92516f2849dbc7c8c1068932f44c9553980
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-01 20:39:19 -0500

    averageOfLevels

--- algorithms.py
+++ algorithms.py
@@ -6563,16 +6563,16 @@ def minSubsequence(self, nums: List[int]) -> List[int]:
         if sum(nums[:i+1]) > total - sum(nums[:i+1]):
             return nums[:i+1]
 
-37. Average of Levels in Binary Tree
+# 37. Average of Levels in Binary Tree
 
-Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+# Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
 
-Example 1:
+# Example 1:
 
-Input: root = [3,9,20,null,null,15,7]
-Output: [3.00000,14.50000,11.00000]
-Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
-Hence return [3, 14.5, 11].
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [3.00000,14.50000,11.00000]
+# Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+# Hence return [3, 14.5, 11].
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
