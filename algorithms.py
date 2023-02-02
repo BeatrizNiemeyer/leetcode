c372069a73e24aa59d24012fa1f8c7b4a0ee85e2
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-01 20:30:00 -0500

    minSubsequence

--- algorithms.py
+++ algorithms.py
@@ -6540,3 +6540,25 @@ def maximumValue(self, strs: List[str]) -> int:
             output.append(len(s))
     
     return max(output)
+
+# 1403. Minimum Subsequence in Non-Increasing Order
+
+# Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
+
+# If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
+
+# Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+
+# Example 1:
+
+# Input: nums = [4,3,10,9,8]
+# Output: [10,9] 
+# Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included. However, the subsequence [10,9] has the maximum total sum of its elements. 
+
+def minSubsequence(self, nums: List[int]) -> List[int]:
+        
+    nums.sort(reverse=True)
+    total = sum(nums)
+    for i in range(len(nums)):
+        if sum(nums[:i+1]) > total - sum(nums[:i+1]):
+            return nums[:i+1]
