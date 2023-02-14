07d0cbe9a64cd0509b62fe65a430b445b4a5b862
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-14 16:53:39 -0500

    mergeSimilarItems

--- algorithms.py
+++ algorithms.py
@@ -6634,3 +6634,41 @@ def finalPrices(self, prices: List[int]) -> List[int]:
     output.append(prices[-1])
 
     return output
+
+# 2363. Merge Similar Items
+
+# You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
+
+#     items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+#     The value of each item in items is unique.
+
+# Return a 2D integer array ret where ret[i] = [valuei, weighti], with weighti being the sum of weights of all items with value valuei.
+
+# Note: ret should be returned in ascending order by value.
+
+# Example 1:
+
+# Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
+# Output: [[1,6],[3,9],[4,5]]
+# Explanation: 
+# The item with value = 1 occurs in items1 with weight = 1 and in items2 with weight = 5, total weight = 1 + 5 = 6.
+# The item with value = 3 occurs in items1 with weight = 8 and in items2 with weight = 1, total weight = 8 + 1 = 9.
+# The item with value = 4 occurs in items1 with weight = 5, total weight = 5.  
+# Therefore, we return [[1,6],[3,9],[4,5]].
+
+def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+
+    d = {}
+    items3 = items1 + items2
+    
+    for value, weight in items3:
+    d[value] = d.get(value, 0 ) + weight
+
+    output = []
+
+    for value, weight in d.items():
+        output.append([value, weight])
+
+    output = sorted(output, key=lambda item:item[0])
+    
+    return output
