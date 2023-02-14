fb097d7cb6540284b6983a33f684f3e3414df7af
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-13 19:56:54 -0500

    finalPrices

--- algorithms.py
+++ algorithms.py
@@ -6600,3 +6600,37 @@ class Solution:
             result.append(level_sum / level_size)
 
         return result
+
+# 1475. Final Prices With a Special Discount in a Shop
+
+# You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+
+# There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
+
+# Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
+
+# Example 1:
+
+# Input: prices = [8,4,6,2,3]
+# Output: [4,2,4,2,3]
+# Explanation: 
+# For item 0 with price[0]=8 you will receive a discount equivalent to prices[1]=4, therefore, the final price you will pay is 8 - 4 = 4.
+# For item 1 with price[1]=4 you will receive a discount equivalent to prices[3]=2, therefore, the final price you will pay is 4 - 2 = 2.
+# For item 2 with price[2]=6 you will receive a discount equivalent to prices[3]=2, therefore, the final price you will pay is 6 - 2 = 4.
+# For items 3 and 4 you will not receive any discount at all.
+
+def finalPrices(self, prices: List[int]) -> List[int]:
+
+    output = []
+
+    for i in range(len(prices) - 1):
+        for j in range(i  + 1, len(prices)):
+            if prices[i] >= prices[j]:
+                output.append(prices[i] - prices[j])
+                break
+            if j == len(prices) - 1:
+                output.append(prices[i])
+
+    output.append(prices[-1])
+
+    return output
