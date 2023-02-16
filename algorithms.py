1fe5e447240c59e3b709045e61be3d51f81f705d
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-15 21:31:04 -0500

    twoOutOfThree

--- algorithms.py
+++ algorithms.py
@@ -6672,3 +6672,44 @@ def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) ->
     output = sorted(output, key=lambda item:item[0])
     
     return output
+
+# 2032. Two Out of Three
+
+# Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+
+
+# Example 1:
+
+# Input: nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+# Output: [3,2]
+# Explanation: The values that are present in at least two arrays are:
+# - 3, in all three arrays.
+# - 2, in nums1 and nums2.
+
+# Example 2:
+
+# Input: nums1 = [3,1], nums2 = [2,3], nums3 = [1,2]
+# Output: [2,3,1]
+# Explanation: The values that are present in at least two arrays are:
+# - 2, in nums2 and nums3.
+# - 3, in nums1 and nums2.
+# - 1, in nums1 and nums3.
+
+# Example 3:
+
+# Input: nums1 = [1,2,2], nums2 = [4,3,3], nums3 = [5]
+# Output: []
+# Explanation: No value is present in at least two arrays.
+
+def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+
+    total_nums= list(set(nums1)) + list(set(nums2)) + list(set(nums3))
+
+    d = Counter(total_nums)
+    output = []
+
+    for key, value in d.items():
+        if value >= 2:
+            output.append(key)
+
+    return output
